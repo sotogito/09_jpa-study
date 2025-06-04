dca43b8839a20f89193e900a7b0d90c246631f38
ibe6 <nxnxnxnx626@gmail.com>
2025-06-04 17:04:54 +0900

    jpa
    jpa 테이블 생성, 영속

---
+++ jungyeon-jpa-workspace/01_persistence-context/src/test/java/com/ibe6/entity/Menu.java
@@ -0,0 +1,29 @@
+package com.ibe6.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+@Entity
+@Table(name="tbl_menu")
+public class Menu {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // 오토인크리먼트랑 같은 기능
+    @Column(name = "menu_code")
+    private int menuCode;
+    @Column(name = "menu_name")
+    private String menuName;
+    @Column(name = "menu_price")
+    private int menuPrice;
+    @Column(name = "category_code")
+    private int categoryCode;
+    @Column(name = "orderable_status")
+    private String orderableStatus;
+}

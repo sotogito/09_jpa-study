6fbfdcbe977894ffc0412719d144818b324deb02
sotogito <sotogitoarchive@gmail.com>
2025-06-09 09:21:57 +0900

    Initial commit

--- sooyeon-jpa-workspace/03_association-mapping/src/test/java/com/sotogito/section04/practice/entity/Team.java
+++ sooyeon-jpa-workspace/03_association-mapping/src/test/java/com/sotogito/section04/practice/entity/Team.java
@@ -3,6 +3,8 @@ package com.sotogito.section04.practice.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -20,5 +22,8 @@ public class Team {
 
     @Column(name = "team_name", nullable = false)
     private String teamName;
+
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    private List<Team> teamList;
 
 }

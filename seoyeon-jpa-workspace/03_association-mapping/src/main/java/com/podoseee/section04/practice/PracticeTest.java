cbf8541c25321b85c2ce09e7c24092b7622ed6bf
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-06-09 14:38:05 +0900

    03_association-mapping 0605 에러발생

---
+++ seoyeon-jpa-workspace/03_association-mapping/src/main/java/com/podoseee/section04/practice/PracticeTest.java
@@ -0,0 +1,20 @@
+package com.podoseee.section04.practice;
+
+public class PracticeTest {
+
+    /*
+        예제.
+        한 팀에는 여러 회원들이 존재하며
+            1           M
+        각 회원들은 각각 하나의 라커를 가지고 있다.
+            1                     1
+
+        < Team >        < Member >          < Locker >
+        team_id(pk)     member_id(pk)       locker_id(pk)
+        team_name       member_name         locker_name
+                        team_id(fk)
+                        locker_id(fk)
+
+     */
+
+}
---
+++ seoyeon-jpa-workspace/03_association-mapping/src/main/java/com/podoseee/section04/practice/Team.java
@@ -0,0 +1,25 @@
+package com.podoseee.section04.practice;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+
+@Entity(name="team")
+@Table(name="tbl_team")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="team_id")
+    private int teamId;
+    @Column(name="team_name")
+    private String teamName;
+
+
+}
